0eb4bc067e01f9f542c56bf89b7454c975d8f5a8
navya2393qa <navyanavy2393@gmail.com>
2026-08-03 11:06:19 +0530

    Added screenshot capture on test failure

--- conftest.py
+++ conftest.py
@@ -1,12 +1,20 @@
+import os
 import pytest
 from playwright.sync_api import sync_playwright
 from config.config import HEADLESS
 
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    rep = outcome.get_result()
+
+    setattr(item, "rep_" + rep.when, rep)
+
+
 @pytest.fixture()
-def page():
+def page(request):
 
     with sync_playwright() as p:
-
         browser = p.chromium.launch(
             headless=HEADLESS,
             slow_mo=1000
@@ -15,5 +23,13 @@ def page():
         page = browser.new_page()
 
         yield page
+
+        # Capture screenshot if test fails
+        if request.node.rep_call.failed:
+            os.makedirs("screenshots", exist_ok=True)
+
+            page.screenshot(
+                path=f"screenshots/{request.node.name}.png"
+            )
 
         browser.close()
